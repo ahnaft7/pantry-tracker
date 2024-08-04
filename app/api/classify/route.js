8679bcbc81d8bb42c8da094e56259947833e6491
Ahnaf Tajwar <tajwara@bu.edu>
2024-08-04 00:01:49 -0400

    added gpt-4o-mini

--- app/api/classify/route.js
+++ app/api/classify/route.js
@@ -24,12 +24,27 @@ export async function POST(req) {
     // Here you need to make sure the API call is correct for image classification
     console.log('Starting image classification...');
     const completion = await openai.chat.completions.create({
-      model: 'meta-llama/llama-3.1-8b-instruct:free',
+      model: 'openai/gpt-4o-mini',
       messages: [
         { role: "system", content: "You are an AI assistant capable of analyzing images based on their URL." },
-        { role: "user", content: `Please analyze and classify the object in this image and give me a 
-          one word answer with the name of the item in the image and don't include punctuation marks: ${imageURL}` }
+        // { role: "user", content: `Please analyze and classify the object in this image and give me a 
+        //   one word answer with the name of the item in the image and don't include punctuation marks: ${imageURL}` }
+        { role: "user", content: [
+          {
+            type: "text",
+            text: `Please analyze and classify the object in this image and give me a \
+            one word answer with the name of the item in the image and don't include punctuation marks: ${imageURL}`,
+          },
+          {
+            type: "image_url",
+            image_url: {
+              url: `${imageURL}`,
+              detail: "low",
+            }
+          }
+        ]}
       ],
+      max_tokens: 1000,
       headers: {
         'HTTP-Referer': `${process.env.VERCEL_URL || 'http://localhost:3000'}`,
         'X-Title': 'Pantry Tracker'
@@ -38,6 +53,8 @@ export async function POST(req) {
     console.log('API response:', completion);
 
     const classifiedItem = completion.choices[0].message.content;
+
+    console.log('API response:', classifiedItem);
 
     return new Response(JSON.stringify({ classifiedItem }), {
       headers: { 'Content-Type': 'application/json' }
